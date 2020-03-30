fa132e51b762ac4c1690db62278bf5fd516ea1ae
Bruno Pagno <brunopagno@usp.br>
2020-03-30 20:28:46 -0300

    Init Commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Welcome to Doodle</title>
+</head>
+<body>
+
+    <div class="mainSection">
+    
+        <div class="logoContainer">
+            <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="Doodle Logo">        
+        </div>
+    
+        <div class="searchContainer">
+            <input class="searchBox" type="text" name="term">
+            <input class="searchButton" type="submit" value="Search">
+
+        </div>
+
+    </div>
+
+
+    
+</body>
+</html>
