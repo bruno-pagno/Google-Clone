1185f2a906233c4c0c2ce93d07f91937cec84217
Bruno Pagno <brunopagno@usp.br>
2020-03-31 16:11:54 -0300

    Interface Ready. Web Crawler in Progress.

---
+++ crawl.php
@@ -0,0 +1,31 @@
+<?php
+include("classes/DomDocumentParser.php");
+
+function createLink($src, $url){
+    echo "SRC: $src<br>";
+    echo "URL: $url<br>";
+} 
+
+function followLinks($url){
+    $parser = new DomDocumentParser($url);
+
+    $linkList = $parser->getLinks();
+    
+    foreach($linkList as $link){
+        $href = $link->getAttribute("href");
+
+        if(strpos($href, "#") !== false){
+            continue;
+        }else if(substr("$href", 0, 11) == "javascript:"){
+            continue;
+        }
+
+        createLink($href, $url);
+
+        // echo $href. "<br>";
+    }
+}
+
+$startUrl = "http://www.reecekenney.com";
+followLinks($startUrl);
+?>
--- search.php
+++ search.php
@@ -1,4 +1,77 @@
 <?php
-echo $_GET["term"];
+    $term = $_GET["term"];
+    if($term == "")exit("You must enter a search term!");
 
+    $type = isset($_GET["type"]) ? $_GET["type"] : "sites";
+
+
+
+
+    
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Welcome to Boogle</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+    <div class="wrapper">
+
+        <div class="header">
+
+            <div class="headerContent">
+                
+                <div class="logoContainer">
+                    <a href="index.php">
+                        <img src="assets/images/logo.png" alt="Boogle Logo">        
+                    </a>
+                </div>
+
+                <div class="searchContainer">
+                    
+                    <form action="search.php" method="GET">
+                    
+                        <div class="searchBarContainer">
+
+                            <input type="text" class="searchBox" name="term">
+                            <button class="searchButton">
+                                <img src="assets/images/icons/searchIcon.png" alt="Search Icon">
+                            </button>
+
+                        </div>
+
+                    </form>
+
+                </div>
+            
+            </div>
+
+            <div class="tabsContainer">
+                
+                <ul class="tabList">
+
+                    <li class="<?php echo $type == 'sites' ? 'active' : '' ?>">
+                        <a href='<?php echo "search.php?term=$term&type=sites"; ?>'>
+                            Sites
+                        </a>
+                    </li>
+
+
+                    <li class="<?php echo $type == 'images' ? 'active' : '' ?>">
+                        <a href='<?php echo "search.php?term=$term&type=images"; ?>'>
+                            Images
+                        </a>
+                    </li>
+
+                </ul>
+
+            </div>
+        
+        </div>
+    </div>
+
+</body>
+</html>
