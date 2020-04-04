286cc137195d58b8dd7a19189bafd803ec342194
Bruno Pagno <brunopagno@usp.br>
2020-04-04 01:02:12 -0300

    Pagination Ready, and also web crawler

--- search.php
+++ search.php
@@ -1,14 +1,14 @@
 <?php
     include("config.php");
+    include("classes/siteResultsProvider.php");
     $term = $_GET["term"];
     if($term == "")exit("You must enter a search term!");
 
     $type = isset($_GET["type"]) ? $_GET["type"] : "sites";
+    $page = isset($_GET["page"]) ? $_GET["page"] : 1;
 
 
 
-
-    
 ?>
 
 <!DOCTYPE html>
@@ -37,7 +37,7 @@
                     
                         <div class="searchBarContainer">
 
-                            <input type="text" class="searchBox" name="term">
+                            <input type="text" class="searchBox" name="term" value="<?php echo $term?>">
                             <button class="searchButton">
                                 <img src="assets/images/icons/searchIcon.png" alt="Search Icon">
                             </button>
@@ -72,6 +72,80 @@
             </div>
         
         </div>
+
+        
+        
+        
+        
+        <div class="mainResultsSection">
+            <?php
+            
+            $pageSize = 20;
+            $resultsProvider = new siteResultsProvider($con);
+            $numResults = $resultsProvider->getNumResults($term);
+            echo "<p class='resultsCount'>$numResults results found</p>";
+
+
+
+
+            echo $resultsProvider->getResultsHtml($page, $pageSize, $term);
+
+            ?>  
+            
+        
+        </div>
+
+        <div class="paginationContainer">
+
+            <div class="pageButtons">  
+            
+                <div class="pageNumberContainer">
+                    <img src="assets/images/pageStart.png" alt="">
+                </div>
+
+
+                <?php
+                    $pagesToShow = 10;
+                    $numPages = ceil($numResults / $pageSize);
+                    $pagesLeft = min($pagesToShow, $numPages);
+
+                    $currentPage = $page - floor($pagesToShow / 2);
+
+                    if($currentPage < 1) $currentPage = 1;
+
+                    if($currentPage + $pagesLeft > $numPages + 1)$currentPage = $numPages + 1 - $pagesLeft;
+
+                    while($pagesLeft != 0 && $currentPage <= $numPages) {
+                        if($currentPage == $page){
+                            echo "<div class='pageNumberContainer'>
+                                    <img src='assets/images/pageSelected.png'>
+                                    <span class='pageNumber'>$currentPage</span> 
+                                </div>";
+                        }else {
+                            echo "<div class='pageNumberContainer'>
+                                <a href='search.php?term=$term&type=$type&page=$currentPage'>
+                                    <img src='assets/images/page.png'> 
+                                    <span class='pageNumber'>$currentPage</span> 
+                                </a>
+                            </div>";
+                                
+                        }
+                        $currentPage++;
+                        $pagesLeft--;
+                    }
+                
+                
+                
+                ?>
+
+                <div class="pageNumberContainer">
+                    <img src="assets/images/pageEnd.png" alt="">
+                </div>
+            
+            
+            </div>
+        </div>
+
     </div>
 
 </body>
